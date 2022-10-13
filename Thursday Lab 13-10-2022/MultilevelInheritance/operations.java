de860734025d59a9779e666dc1756895f24c6636
Simran VIshwakarma <114000549+simran1061@users.noreply.github.com>
2022-10-13 19:55:50 +0530

    Update operations.java

--- Thursday Lab 13-10-2022/MultilevelInheritance/operations.java
+++ Thursday Lab 13-10-2022/MultilevelInheritance/operations.java
@@ -1,10 +1,10 @@
 
-public class operations extends sumofdig {
+public class operations extends sumofdig { //inheriting the class
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		rev();
-		sums();
+		rev(); //calling a function
+		sums(); //calling a function
 
 	}
 
