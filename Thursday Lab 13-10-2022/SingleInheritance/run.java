99d8e2722f4a27b308db6352c81a8c39b08f0db6
Simran VIshwakarma <114000549+simran1061@users.noreply.github.com>
2022-10-13 19:43:21 +0530

    Add files via upload

---
+++ Thursday Lab 13-10-2022/SingleInheritance/run.java
@@ -0,0 +1,8 @@
+public class run extends single {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    palindrome();
+  }
+
+}
---
+++ Thursday Lab 13-10-2022/SingleInheritance/single.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class single 
+{
+
+  public static void palindrome() {
+    // TODO Auto-generated method stub
+    //Initializing number reverse number and remainder
+        int num,revnum=0,rem;
+        //Getting input from user
+        System.out.println("Enter the Number : ");
+        Scanner sc = new Scanner(System.in);
+        num=sc.nextInt();
+        //Initializing Original number
+        int orinum=num;
+        //Using while loop to reverse the number
+        while(num != 0) {
+          rem=num%10;
+          revnum=revnum*10+rem;
+          num /= 10;
+        }
+        //Check if Original number matches the reverse number or not i.e. palindrome or not
+        if (orinum == revnum) {
+              System.out.println(orinum + " is a Palindrome.");
+            }
+            else {
+              System.out.println(orinum + " is not a Palindrome.");
+            }
+  }
+
+}
