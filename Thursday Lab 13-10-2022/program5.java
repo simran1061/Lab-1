76e4ba4518228e6981568f084c274086eabb709e
Simran VIshwakarma <114000549+simran1061@users.noreply.github.com>
2022-10-13 19:48:06 +0530

    Update program5.java

--- Thursday Lab 13-10-2022/program5.java
+++ Thursday Lab 13-10-2022/program5.java
@@ -1,3 +1,6 @@
+/*Q4)
+Write a program to accpet the positive numbers  from user and display the sum of that numbers using loop.
+ */ 
 import java.util.*;
 
 public class program5 {
@@ -8,18 +11,18 @@ public class program5 {
 	    Scanner sc = new Scanner(System.in);
 	    System.out.println("Enter the amount of positive numbers you want to add :");
 	    n=sc.nextInt();
-	    int arr[] = new int[n];
+	    int arr[] = new int[n]; // taking array size from user and assigning to n integer
 	    System.out.println("Enter the Numbers");
-	        for(int i=0;i<n;i++)
+	        for(int i=0;i<n;i++) // using loop to assign int values
 	        {
 	            arr[i]=sc.nextInt();
 	        }
 	        int total=0;
 	        for(int i=0;i<n;i++)
 	        {
-	            total=total+arr[i];
+	            total=total+arr[i]; //addition of numbers in a array
 	        }
-	        //Display the total marks
+	        //Display the total numbers
 	        System.out.println("The Sum of positive number is "+total);
 	}
 }
